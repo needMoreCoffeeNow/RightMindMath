7ee412780616a2474a002d11e62a927e629ac5d2
needMoreCoffeeNow <58310888+needMoreCoffeeNow@users.noreply.github.com>
2022-06-17 17:13:45 -0700

    adjust spacing && add console.log when deleting cache key

--- app/serviceworker_en_us.js
+++ app/serviceworker_en_us.js
@@ -16,42 +16,43 @@ const assets = [
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil((async () => {
-    const cache = await caches.open(cache_name);
-    await cache.addAll(assets);
-  })());
+    e.waitUntil((async () => {
+      const cache = await caches.open(cache_name);
+      await cache.addAll(assets);
+    })());
 });
 
 
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cache_name);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(assets);
-  })());
+    console.log('[ServiceWorker] Install');
+    e.waitUntil((async () => {
+        const cache = await caches.open(cache_name);
+        console.log('[ServiceWorker] Caching all: app shell and content');
+        await cache.addAll(assets);
+    })());
 });
 
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) { return r; }
-    const response = await fetch(e.request);
-    const cache = await caches.open(cache_name);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
+    e.respondWith((async () => {
+        const r = await caches.match(e.request);
+        console.log(`[ServiceWorker] Fetching resource: ${e.request.url}`);
+        if (r) { return r; }
+        const response = await fetch(e.request);
+        const cache = await caches.open(cache_name);
+        console.log(`[ServiceWorker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+    })());
 });
 
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keyList) => {
-    return Promise.all(keyList.map((key) => {
-      if (key === cache_name) { return; }
-      return caches.delete(key);
-    }))
-  }));
+    e.waitUntil(caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+            if (key === cache_name) { return; }
+            console.warn(`[ServiceWorker] Deleting Cache Key: ${key}`);
+            return caches.delete(key);
+        }))
+    }));
 });
