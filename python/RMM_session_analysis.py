0775960e6672865412e270bfb8020fc410efcef9
needMoreCoffeeNow <58310888+needMoreCoffeeNow@users.noreply.github.com>
2022-08-03 07:00:19 -0700

    add function to create dataframe structure compatible with Pandas dataframe = {'idsession':[a,b,c,etc], ...} where abc =  list of values one per line read from file

--- python/RMM_session_analysis.py
+++ python/RMM_session_analysis.py
@@ -2,6 +2,8 @@ import json
 import urllib.parse
 from random import randint
 import datetime
+#import matplotlib.pyplot as plt
+import pandas as pd
 
 def randomizedNotes():
     limit_1 = 7
@@ -34,7 +36,8 @@ class ProcessJsonFile():
                         's1':0, 's2':0, 's3':0,
                         'm1':[0,0], 'm2':0, 'm3':0,
                         'd3':0}
-        self.dframe_dict = {
+        self.dframe = {} # pandas dataframe built as file is read using df_rec
+        self.df_rec = {
             # r_str vars start
             'idlevel_rstr' : None,
             'steps_total' : None,
@@ -90,47 +93,46 @@ class ProcessJsonFile():
 
     def parseRstr(self, r_str, tstamp, time):
         not_asm = {'d3':True, 'm2':True}
-        my_fr = self.dframe_dict.copy()
         parts = r_str.split('^')
         vars = parts[0].split('.')
         # strip the b/c from M2b M2c
-        my_fr['idlevel_rstr'] = vars[0]
+        self.df_rec['idlevel_rstr'] = vars[0]
         idlevel = vars[0][0:2]
         if (idlevel == 'm2'):
-            my_fr['m2_basic'] = vars[0][2:3] == 'b'
+            self.df_rec['m2_basic'] = vars[0][2:3] == 'b'
         if not idlevel in not_asm:
-            my_fr['steps_total'] = int(vars[1])
-            my_fr['steps_count'] = int(vars[2]) + 1 # change to 1=start index
+            self.df_rec['steps_total'] = int(vars[1])
+            self.df_rec['steps_count'] = int(vars[2]) + 1 # change to 1=start index
             vars = parts[1].split('|')
-            my_fr['op1'] = int(vars[0])
-            my_fr['op2'] = int(vars[1])
-            my_fr['answer'] = int(vars[2])
-            my_fr['op'] = vars[3]
+            self.df_rec['op1'] = int(vars[0])
+            self.df_rec['op2'] = int(vars[1])
+            self.df_rec['answer'] = int(vars[2])
+            self.df_rec['op'] = vars[3]
             if idlevel == 'm1':
-                my_fr['chunk_count'] = int(parts[4])
+                self.df_rec['chunk_count'] = int(parts[4])
             else:
-                my_fr['chunk_count'] = int(parts[3])
+                self.df_rec['chunk_count'] = int(parts[3])
         else:
             if idlevel == 'd3':
                 vars = parts[1].split('/')
-                my_fr['op1'] = int(vars[0])
-                my_fr['op2'] = int(vars[1])
-                my_fr['answer'] = float(my_fr['op1']) / float(my_fr['op2'])
-                my_fr['op'] = '/'
+                self.df_rec['op1'] = int(vars[0])
+                self.df_rec['op2'] = int(vars[1])
+                self.df_rec['answer'] = float(self.df_rec['op1']) / float(self.df_rec['op2'])
+                self.df_rec['op'] = '/'
             else:
                 vars = parts[1].split('x')
-                my_fr['op1'] = int(vars[0])
-                my_fr['op2'] = int(vars[1])
-                my_fr['answer'] = my_fr['op1'] * my_fr['op2']
-                my_fr['op'] = 'x'
+                self.df_rec['op1'] = int(vars[0])
+                self.df_rec['op2'] = int(vars[1])
+                self.df_rec['answer'] = self.df_rec['op1'] * self.df_rec['op2']
+                self.df_rec['op'] = 'x'
         if idlevel == 'm':
-            my_fr['ordered'] = parts[2] == 'true'
-        if my_fr['op1'] < 0: my_fr['neg_op1'] = 1
-        if my_fr['op2'] < 0: my_fr['neg_op2'] = 1
-        if my_fr['answer'] < 0: my_fr['neg_ans'] = 1
+            self.df_rec['ordered'] = parts[2] == 'true'
+        if self.df_rec['op1'] < 0: self.df_rec['neg_op1'] = 1
+        if self.df_rec['op2'] < 0: self.df_rec['neg_op2'] = 1
+        if self.df_rec['answer'] < 0: self.df_rec['neg_ans'] = 1
         if idlevel in not_asm:
-            my_fr['chunk_count'] = int(parts[2])
-            return my_fr # no answers for multi-digit problems
+            self.df_rec['chunk_count'] = int(parts[2])
+            return self.df_rec # no answers for multi-digit problems
         # break out the answers for 1-digit problems
         if not idlevel ==  'm1':
             answers = parts[2].split('|')
@@ -140,7 +142,7 @@ class ProcessJsonFile():
         # timet = time problem was entered - tstamp
         # so to get tstamp of time problem was entered we do this math...
         ts_last = tstamp - time
-        my_fr['problem_start'] = ts_last
+        self.df_rec['problem_start'] = ts_last
         i = 0
         for answer in sorted(answers):
             vars = answer.split('_')
@@ -148,18 +150,17 @@ class ProcessJsonFile():
             ts_ans = int(vars[0])
             delta = ts_ans - ts_last
             vname = 'ans_elapsed_%d%d' % (i, (i+1))
-            my_fr[vname] = delta
+            self.df_rec[vname] = delta
             ts_last = ts_ans
             i += 1
         if idlevel == 's2' or idlevel == 's3':
-            sop1 = my_fr['op1']
-            sop2 = my_fr['op2']
-            if (sop1 % 10) < (sop2 % 10): my_fr['borrow_01'] = 1
+            sop1 = self.df_rec['op1']
+            sop2 = self.df_rec['op2']
+            if (sop1 % 10) < (sop2 % 10): self.df_rec['borrow_01'] = 1
             if idlevel == 's3':
                 sop1 = int(sop1 / 10)
                 sop2 = int(sop2 / 10)
-                if (sop1 % 10) < (sop2 % 10): my_fr['borrow_10'] = 1
-        return my_fr
+                if (sop1 % 10) < (sop2 % 10): self.df_rec['borrow_10'] = 1
 
     def buildDataFrame(self):
         id_last = '0_0'
@@ -175,63 +176,69 @@ class ProcessJsonFile():
             ######print('-'*100)
             ######print(rec)
             ######print(rec['idsession'], rec['r_str'])
-            print('-'*100)
-            print(rec['idsession'])
-            print(rec['r_str'])
-            # run parseRstr() first to get a copy() of this.dframe_dict
-            my_fr = self.parseRstr(rec['r_str'], rec['tstamp'], rec['time'])
-            my_fr['idproblem'] = id_this
+            ######print('-'*100)
+            ######print(rec['idsession'])
+            ######print(rec['r_str'])
+            self.parseRstr(rec['r_str'], rec['tstamp'], rec['time'])
+            self.df_rec['idproblem'] = id_this
             # idsession tstamp is basis for date inputs
             ids_tstamp = int(vars[0])
-            my_fr['idsession_tstamp'] = ids_tstamp
+            self.df_rec['idsession_tstamp'] = ids_tstamp
             dt = datetime.date.fromtimestamp(int(ids_tstamp/1000))
-            my_fr['date_date'] = dt.strftime('%Y%m%d')
-            my_fr['date_weekday'] = dt.strftime('%A')[0:3] #Mon, Tue, Wed, etc.
+            self.df_rec['date_date'] = dt.strftime('%Y%m%d')
+            self.df_rec['date_weekday'] = dt.strftime('%A')[0:3] #Mon, Tue, Wed, etc.
             delta = date_now - dt
-            my_fr['date_days'] = delta.days
+            self.df_rec['date_days'] = delta.days
             if len(vars) == 2:
-                my_fr['idproblem_count'] = 1
+                self.df_rec['idproblem_count'] = 1
             else:
                 # record idproblem_count for first rec in multi step problem
                 if int(vars[2]) == 1:
-                    my_fr['idproblem_count'] = 1
+                    self.df_rec['idproblem_count'] = 1
             for key, val in rec.items():
-                if key in my_fr:
-                    my_fr[key] = val
+                if key in self.df_rec:
+                    self.df_rec[key] = val
             if idlevel == 'm2' or idlevel == 'd3':
-                my_fr['problem_m2d3'] = 1
-                my_fr['idsession_count'] = int(vars[1])
+                self.df_rec['problem_m2d3'] = 1
+                self.df_rec['idsession_count'] = int(vars[1])
             if idlevel == 'a1' or idlevel == 's1' or idlevel == 'm1':
-                my_fr['problem_1digit'] = 1
-                my_fr['idsession_count'] = int(vars[1])
+                self.df_rec['problem_1digit'] = 1
+                self.df_rec['idsession_count'] = int(vars[1])
             # accumulate multi idsession times / tries -- start
             if idlevel in self.p_multi:
-                my_fr['problem_multi'] = 1
+                self.df_rec['problem_multi'] = 1
                 if id_this != id_last: # start of multi problem
-                    print('---first')
-                    my_fr['total_time'] = rec['time']
-                    my_fr['total_tries'] = rec['tries']
-                    my_fr['idsession_count'] = int(vars[1])
+                    ######print('---first')
+                    self.df_rec['total_time'] = rec['time']
+                    self.df_rec['total_tries'] = rec['tries']
+                    self.df_rec['idsession_count'] = int(vars[1])
                 else:
-                    print('---next')
-                    my_fr['total_time'] = rec['time'] + time_last
-                    my_fr['total_tries'] = rec['tries'] + tries_last
+                    ######print('---next')
+                    self.df_rec['total_time'] = rec['time'] + time_last
+                    self.df_rec['total_tries'] = rec['tries'] + tries_last
                 time_last += rec['time']
                 tries_last += rec['tries']
                 ######print(rec['time'], rec['tries'], "rec['time']", "rec['tries']")
                 ######print(time_last, tries_last, 'time_last', 'tries_last')
-                ######print(my_fr['total_time'], my_fr['total_tries'], "my_fr['total_time']", "my_fr['total_tries']")
+                ######print(self.df_rec['total_time'], self.df_rec['total_tries'], "self.df_rec['total_time']", "self.df_rec['total_tries']")
             id_last = id_this
             # accumulate multi idsession times / tries -- end
-            print('-'*100)
-            for key, val in sorted(my_fr.items()):
-                if val is None: continue
-                print(key, '---', val)
-            print('-'*100)
-            for key, val in sorted(my_fr.items()):
-                if not val is None: continue
-                ######print(key, '---', val)
-        return my_fr
+            ######print('-'*100)
+            ######for key, val in sorted(self.df_rec.items()):
+            ######    if val is None: continue
+            ######    print(key, '---', val)
+            ######print('-'*100)
+            ######for key, val in sorted(self.df_rec.items()):
+            ######    if not val is None: continue
+            ######    ######print(key, '---', val)
+            ######print(len(self.df_rec))
+            self.dframeAddRec()
+
+    def dframeAddRec(self):
+        for key, val in sorted(self.df_rec.items()):
+            array = self.dframe.setdefault(key, [])
+            array.append(val)
+            self.dframe[key] = array
 
     def readFile(self):
         uniques = {}
@@ -327,4 +334,5 @@ if __name__ == '__main__':
     pjf.readFile()
     pjf.processLines()
     pjf.printLinesStats()
-    my_fr = pjf.buildDataFrame()
+    pjf.buildDataFrame()
+    dataframe = pd.DataFrame(pjf.dframe)
